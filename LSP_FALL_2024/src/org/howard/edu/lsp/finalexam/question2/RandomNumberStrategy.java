52074a4e2e86e993fa3464e134660b7196f812eb
DalvinT <132528413+DalvinT@users.noreply.github.com>
2024-12-05 20:01:45 -0500

    Create RandomNumberStrategy.java

---
+++ LSP_FALL_2024/src/org/howard/edu/lsp/finalexam/question2/RandomNumberStrategy.java
@@ -0,0 +1,12 @@
+package org.howard.edu.lsp.finalexam.question2;
+
+/**
+ * Strategy interface for generating random numbers.
+ */
+public interface RandomNumberStrategy {
+    /**
+     * Generates a random positive integer.
+     * @return a random positive integer
+     */
+    int generate();
+}
